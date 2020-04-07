115972b919cf115d6e44737a1d58509eff0a4841
Christian Beretta <beretta.christian2602@gmail.com>
2020-04-07 17:33:50 +0200

    inizio milestone 1

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(document).ready( function (){
+    var arrayMesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+    
+
+    // creo oggetto intermedio da rimepire con dati per grafico
+    var objIntermedioMesi = {};
+    chiamataApi();
+    console.log(objIntermedioMesi);
+
+
+    var labelsPC = [];
+    var dataPC = [];
+
+
+    for (var key in objIntermedioMesi) {
+        console.log(key);
+    }
+
+
+    // stampa grafico chart.js
+    // grafico-line per andamento vendite mensili complessive
+    // var chartLine = new Chart($('#grafico-line'), {
+    //     type: 'line',
+    //     data: {
+    //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    //         datasets: [{
+    //             label: 'My First dataset',
+    //             backgroundColor: 'rgb(255, 99, 132)',
+    //             borderColor: 'rgb(255, 99, 132)',
+    //             data: [0, 10, 5, 2, 20, 30, 45]
+    //         }]
+    //     }
+    // });
+
+    // grafico-pie per andamento vendite annuali per venditore
+    // var chartPie = new Chart($('#grafico-pie'), {
+    //     type: 'pie',
+    //     data: {
+    //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    //         datasets: [{
+    //             label: 'My First dataset',
+    //             backgroundColor: 'rgb(255, 99, 132)',
+    //             borderColor: 'rgb(255, 99, 132)',
+    //             data: [0, 10, 5, 2, 20, 30, 45]
+    //         }]
+    //     }
+    // });
+
+    function chiamataApi() {
+        $.ajax({
+            url: 'http://157.230.17.132:4002/sales',
+            method: 'GET',
+            success: function (data) {
+                getDati(data);
+            },
+            error: function (err) {
+                alert('errore richiesta');
+            }
+        });
+    };
+
+    function getDati(array) {
+        for (var i = 0; i < array.length; i++) {
+            var oggettoSingolo = array[i];
+            var dataVendita = oggettoSingolo.date;
+            var meseVendita = moment(dataVendita, "DD-MM-YYYY").clone().format('MMMM');
+            if (objIntermedioMesi[meseVendita] === undefined) {
+                objIntermedioMesi[meseVendita] = 0;
+            }
+            objIntermedioMesi[meseVendita] += oggettoSingolo.amount;
+        }
+    }
+
+});
