473e8c8cb4965a8ebcf3fccc064f41b8eb5e5de1
Christian Beretta <beretta.christian2602@gmail.com>
2020-04-07 18:55:49 +0200

    milestone 1 completata + problema
    problema visualizzazione grafici (serve aprire l'inspector per visulizzarli entrambi) + problema cicli for e for in che non vengono eseguiti!

--- js/main.js
+++ js/main.js
@@ -1,57 +1,60 @@
 
-var arrayMesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-
-// creo oggetto intermedio da rimepire con dati per grafico
+// creo oggetto intermedio da riempire con dati per grafico
 var objIntermedioMesi = {};
-chiamataApi();
-console.log(objIntermedioMesi);
+var dataMesi = [];
+getVenditeMensili();
 
+var objIntermedioVenditori = {};
+var labelsVenditori = [];
+var dataVenditori = [];
+var totaleVendite = 0;
+getVenditeVenditori();
 
-var labelsPC = [];
-var dataPC = [];
-
-for (var pippo in objIntermedioMesi) {
-    console.log(pippo);
+// !!!!!!!!!!! NON VIENE ESEGUITO IL CICLO !!!!!!!!!!
+for (var i = 0; i < dataVenditori.length; i++) {
+    totaleVendite += dataVenditori[i];
+    console.log(totaleVendite);
 }
+for (var i = 0; i < dataVenditori.length; i++) {
+    dataVenditori[i] = (dataVenditori[i] / totaleVendite);
+}
+console.log(totaleVendite);
 
 
-
-
-// stampa grafico chart.js
 // grafico-line per andamento vendite mensili complessive
-// var chartLine = new Chart($('#grafico-line'), {
-//     type: 'line',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     }
-// });
+var lineChart = new Chart($('#grafico-line'), {
+    type: 'line',
+    data: {
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+        datasets: [{
+            label: 'Vendite Mensili',
+            borderColor: 'rgb(255, 99, 132)',
+            data: dataMesi,
+            lineTension: 0,
+        }]
+    }
+});
 
 // grafico-pie per andamento vendite annuali per venditore
-// var chartPie = new Chart($('#grafico-pie'), {
-//     type: 'pie',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     }
-// });
+var pieChart = new Chart($('#grafico-pie'), {
+    type: 'pie',
+    data: {
+        labels: labelsVenditori,
+        datasets: [{
+            label: 'Vendite Mensili',
+            data: dataVenditori,
+            backgroundColor: ['lightgreen', 'lightblue', 'lightcoral', 'yellow']
+        }]
+    }
+});
 
-function chiamataApi() {
+
+function getVenditeMensili() {
     $.ajax({
         url: 'http://157.230.17.132:4002/sales',
         method: 'GET',
         success: function (data) {
-            getDati(data);
+            getDatiMesi(data);
         },
         error: function (err) {
             alert('errore richiesta');
@@ -59,15 +62,47 @@ function chiamataApi() {
     });
 };
 
-function getDati(array) {
+function getVenditeVenditori() {
+    $.ajax({
+        url: 'http://157.230.17.132:4002/sales',
+        method: 'GET',
+        success: function (data) {
+            getDatiVenditori(data);
+        },
+        error: function (err) {
+            alert('errore richiesta');
+        }
+    });
+};
+
+// funzione per ricavare i dati delle vendite mensili e popolare la variabile dataPC
+function getDatiMesi(array) {
     for (var i = 0; i < array.length; i++) {
         var oggettoSingolo = array[i];
-        var dataVendita = oggettoSingolo.date;
-        var meseVendita = moment(dataVendita, "DD-MM-YYYY").clone().format('MMMM');
-        // var meseVendita = moment(dataVendita, "DD-MM-YYYY").clone().mont(); // ottengo i numeri dei mesi che escono ordinati nell'oggetto
+        var giornoVendita = oggettoSingolo.date;
+        var meseVendita = moment(giornoVendita, "DD-MM-YYYY").clone().month(); // ottengo i numeri dei mesi che escono già ordinati nell'oggetto
         if (objIntermedioMesi[meseVendita] === undefined) {
             objIntermedioMesi[meseVendita] = 0;
         }
         objIntermedioMesi[meseVendita] += oggettoSingolo.amount;
+    }
+    for (var key in objIntermedioMesi) {
+        // labelsPC.push(key);
+        dataMesi.push(objIntermedioMesi[key]);
+    }
+}
+
+function getDatiVenditori(array) {
+    for (var i = 0; i < array.length; i++) {
+        var oggettoSingolo = array[i];
+        var venditore = oggettoSingolo.salesman;
+        if (objIntermedioVenditori[venditore] === undefined) {
+            objIntermedioVenditori[venditore] = 0;
+        }
+        objIntermedioVenditori[venditore] += oggettoSingolo.amount;
+    }
+    for (var key in objIntermedioVenditori) {
+        labelsVenditori.push(key);
+        dataVenditori.push(objIntermedioVenditori[key]);
     }
 }
